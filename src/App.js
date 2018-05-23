758ff4c708e811308a0df1e0bc9fd512d6d489f3
Gonzodamus <Dick@TheDickWard.com>
2018-05-23 12:07:31 -0400

    numbers update according to rules set out

--- src/App.js
+++ src/App.js
@@ -6,17 +6,55 @@ import specialData from "./special-data";
 
 class App extends Component {
   state = {
-    pointsRemaining: 0
+    specialData: specialData
   };
   render() {
-    const categories = specialData.map(category => {
-      return <Category key={category.name} category={category} />;
+    const handlePoints = (categoryName, direction) => {
+      const specialData = this.state.specialData.map(category => {
+        if (
+          category.name === categoryName &&
+          direction === "right" &&
+          category.points < 10 &&
+          pointsRemaining > 0
+        ) {
+          category.points += 1;
+        } else if (
+          category.name === categoryName &&
+          direction === "left" &&
+          category.points > 0
+        ) {
+          category.points -= 1;
+        }
+        return category;
+      });
+      this.setState({ specialData });
+    };
+
+    const categories = this.state.specialData.map(category => {
+      return (
+        <Category
+          key={category.name}
+          category={category}
+          handlePoints={handlePoints}
+        />
+      );
     });
+
+    const pointMap = specialData.map(category => {
+      return category.points;
+    });
+
+    const pointsUsed = pointMap.reduce((a, b) => {
+      return a + b;
+    });
+
+    const pointsRemaining = 28 - pointsUsed;
+
     return (
       <div>
         <div className="headline">You're S.P.E.C.I.A.L.</div>
         {categories}
-        <PointsRemaining pointsRemaining={this.state.pointsRemaining} />
+        <PointsRemaining pointsRemaining={pointsRemaining} />
       </div>
     );
   }
--- src/category.js
+++ src/category.js
@@ -2,15 +2,31 @@ import React from "react";
 
 class Category extends React.Component {
   render() {
+    const handleClick = e => {
+      this.props.handlePoints(
+        e.target.getAttribute("category-name"),
+        e.target.id
+      );
+    };
     const { category } = this.props;
     return (
       <div className="category-box">
         <div className="category-background">
           <div className="category-name">{category.name}</div>
           <div className="arrow-container">
-            <i className="fas fa-arrow-left arrow" />
+            <i
+              className="fas fa-arrow-left arrow"
+              id="left"
+              category-name={category.name}
+              onClick={handleClick}
+            />
             <div className="category-value">{category.points}</div>
-            <i className="fas fa-arrow-right arrow" />
+            <i
+              className="fas fa-arrow-right arrow"
+              id="right"
+              category-name={category.name}
+              onClick={handleClick}
+            />
           </div>
         </div>
       </div>
